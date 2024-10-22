f56a100e06d9b405df7bd71912d979553b30f9f7
Fernando Palacios <fernando.palacios@ids.com.mx>
2024-10-22 17:16:27 -0600

    Se agrega codigo javascript para calcular edad

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = prompt("Ingresa el año en que naciste (4 digitos):", "1980");
+let m = prompt("Ingresa el mes en que naciste (1-12):", "8");
+let d = prompt("Ingresa el día en que naciste (1-31):", "15");
+
+let fecha_nacimiento = new Date();
+fecha_nacimiento.setFullYear(parseInt(a), parseInt(m) - 1, parseInt(d));
+
+var edad = Math.floor( (Date.now() - fecha_nacimiento.getTime()) / (60 * 60 * 24 * 1000 * 365.25) );
+
+const month = fecha_nacimiento.toLocaleString('default', { month: 'long' });
+
+alert("Tu fecha de naciemiento es: " + d + " de " + month + " del año " + a);
+alert("Y tienes " + edad + " años.");
